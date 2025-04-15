3713e3ad9ea8951fd7f81cdd09f70b55ed7ad006
[200~essadikine.fst@uhp.ac.ma <essadikine.fst@uhp.ac.ma>
2025-04-15 23:38:49 +0100

    new

--- app/api/businesses.js
+++ app/api/businesses.js
@@ -1,6 +1,5 @@
 import clientPromise from '../../app/lib/mongodb';
 
-
 export default async function handler(req, res) {
   const client = await clientPromise;
   const db = client.db('my-scraper-app');
